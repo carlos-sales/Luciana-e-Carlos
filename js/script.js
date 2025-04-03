79e42e2eb298dc3671f255a55afd47f83d198208
Carlos Alberto Sales <carlos.gsales27@gmail.com>
2025-04-03 14:37:39 -0300

    menu e SPA

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", 
+    function()
+    {
+        goTo('menu.html')
+    }
+);
+
+function goTo(page)
+{
+    fetch(page)
+        .then(response => response.text())
+        .then(html => document.getElementById("content").innerHTML = html);
+}
